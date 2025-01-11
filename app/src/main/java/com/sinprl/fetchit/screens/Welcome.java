33011d05eafed2b72aa09e08a2a648e6be3340d2
Prashant Alhat <prashant.alhat@gmail.com>
2025-01-11 21:49:48 +0530

    welcome screen added

---
+++ app/src/main/java/com/sinprl/fetchit/screens/Welcome.java
@@ -0,0 +1,36 @@
+package com.sinprl.fetchit.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.sinprl.fetchit.R;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Welcome.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
